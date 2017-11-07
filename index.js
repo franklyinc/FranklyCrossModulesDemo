cf656075a5c2a97a55f2973706202eab9e0a5de8
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-07 13:22:59 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import React, { Component, PropTypes } from 'react';
+import ModuleDemo from 'franklyinc/FranklyModulesDemo';
+
+class ModuleCrossDemo extends Component {
+
+  render(){
+	  
+    return (
+      <div className='FranklyModulesCrossDemo'>
+	      <ModuleDemo>test</ModuleDemo>
+      </div>
+    );
+	  
+  }
+}
+
+export default ModuleCrossDemo;
