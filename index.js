d331852dc4b86ae1d177f76eebacdcb9c4d1a654
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-14 15:18:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 // import ModuleDemo from 'franklyinc/FranklyModulesDemo';
 import Dep from './dep';
+import folderTest from './depFolder/folderTest';
 
 class ModuleCrossDemo extends Component {
 
