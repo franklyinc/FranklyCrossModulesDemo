0f54275b7c059792e4ae3f92b1388769d4d5d8fd
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-14 15:17:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React, { Component, PropTypes } from 'react';
 // import ModuleDemo from 'franklyinc/FranklyModulesDemo';
+import Dep from './dep';
 
 class ModuleCrossDemo extends Component {
 
@@ -8,6 +9,7 @@ class ModuleCrossDemo extends Component {
     return (
       <div className='FranklyModulesCrossDemo'>
 	    This is a child "ModuleDemo" component:<br/>
+	    <Dep></Dep>
 	    {/* <ModuleDemo text="test"></ModuleDemo> --> */ }
       </div>
     );
