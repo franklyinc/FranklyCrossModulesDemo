20c58939fb836092345c56ad72f19c6608b02ca2
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-14 15:16:47 -0500

    Create dep.js

---
+++ dep.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+class Dep extends Component {
+
+  render(){
+	  
+    return (
+      <div>DEP</div>
+    );
+	  
+  }
+}
+
+export default Dep;
