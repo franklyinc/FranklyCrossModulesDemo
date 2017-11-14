5f28223cf19d080a84c9630a50eac9a369e5afa2
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2017-11-14 15:18:19 -0500

    Create folderTest.js

---
+++ depFolder/folderTest.js
@@ -0,0 +1,14 @@
+import React, { Component, PropTypes } from 'react';
+
+class folderTest extends Component {
+
+  render(){
+	  
+    return (
+      <div>folderTest</div>
+    );
+	  
+  }
+}
+
+export default folderTest;
