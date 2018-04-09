174e91d5a120df7f75e4f3a2503efc6ebc0de63d
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-09 14:20:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,11 @@ import React, { Component, PropTypes } from 'react';
 class ModuleCrossDemo extends Component {
   constructor (props) { // gives us acces to props, fires long before page load
     super(props) // assigns props to this.props
-
-
-
-    this.imgUrl = 'http://ftpcontent.worldnow.com/kotv/test/don/build/';
+//     this.imgUrl = 'http://ftpcontent.worldnow.com/kotv/test/don/build/';
     this.state = {
       stories: []
     } /* great place to assign default state */;
-    this.stories = props.FRN_rawResponses;
+//     this.stories = props.FRN_rawResponses;
 
   }
 	
