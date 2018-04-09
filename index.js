5423a02fe138e77afbb68309ba450797639570a0
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-09 13:58:50 -0400

    adding ajax

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import React, { Component, PropTypes } from 'react';
-import ModuleDemo from 'franklyinc/FranklyModulesDemo';
+// import ModuleDemo from 'franklyinc/FranklyModulesDemo';
 // import Dep from './dep';
 // import folderTest from './depFolder/folderTest';
+
+ajax1 = (url,callback) => {
+    let req = new XMLHttpRequest();
+    req.open('GET', url);
+    req.onload = function() {
+        if (req.status === 200) {
+            callback(req.response);
+        } else {
+            new Error(req.statusText);
+        }
+    };
+    req.onerror = function() {
+        new Error('Network error');
+    };
+    req.send();
+  }
 
 class ModuleCrossDemo extends Component {
 
@@ -12,7 +28,7 @@ class ModuleCrossDemo extends Component {
 	    This is a child "ModuleDemo" component:<br/>
 	    {/* <<Dep></Dep> */ }
 	    {/* <<folderTest></folderTest> */ }
-	    <ModuleDemo text="test"></ModuleDemo>
+	    {/* <ModuleDemo text="test"></ModuleDemo> */ }
       </div>
     );
 	  
